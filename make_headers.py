afbc43372078cfb69e74955875220798094447d3
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-09-29 19:59:34 +0000

    Adding tarfile member sanitization to extractall()

--- make_headers.py
+++ make_headers.py
@@ -43,7 +43,29 @@ def _generate_headers(ver: str, verbose: bool):
     try:
         with tarfile.open(ntf.name, 'r') as tar, tempfile.TemporaryDirectory() as tmpdir:
             dst = pathlib.Path(tmpdir)
-            tar.extractall(path=dst)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, path=dst)
             logger.info(f'Finished extracting to {dst / archive_name} in {time() - t0:.2f} seconds')
 
             # configure build by calling bootstrap.sh
